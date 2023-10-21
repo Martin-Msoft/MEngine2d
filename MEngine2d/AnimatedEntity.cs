58a97bd089b45ba7e584e5c392503a5056543bc0
Martin Ouwehand <martin.ouwehand@live.nl>
2023-10-21 21:06:59 +0200

    Project created.

---
+++ MEngine2d/AnimatedEntity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MEngine2d
+{
+    public class AnimatedEntity
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private int currentFrame = 0;
+        private List<BitmapSource> frames = new List<BitmapSource>();
+        private int animationSpeed = 20;
+        private int animationCounter = 0;
+
+        public AnimatedEntity(BitmapImage spriteSheet, int frameWidth, int frameHeight)
+        {
+            // Extract individual frames from the sprite sheet
+            for(int y = 0; y < spriteSheet.PixelHeight; y += frameHeight)
+            {
+                for(int x = 0; x < spriteSheet.PixelWidth; x += frameWidth)
+                {
+                    CroppedBitmap frame = new CroppedBitmap(spriteSheet, new Int32Rect(x, y, frameWidth, frameHeight));
+                    frames.Add(frame);
+                }
+            }
+        }
+
+        public void Update()
+        {
+            // Update logic here, e.g., move the entity, handle collisions, etc.
+        }
+
+        public void Animate()
+        {
+            animationCounter++;
+            if(animationCounter >= animationSpeed)
+            {
+                currentFrame = (currentFrame + 1) % frames.Count;
+                animationCounter = 0;
+            }
+        }
+
+        public void Draw(WriteableBitmap writeableBitmap, int x, int y)
+        {
+            Rect destRect = new Rect(x, y, frames[currentFrame].PixelWidth, frames[currentFrame].PixelHeight);
+
+            // Clear the previous frame by filling the rectangle with a transparent color
+            writeableBitmap.FillRectangle((int)destRect.X, (int)destRect.Y, (int)(destRect.X + destRect.Width), (int)(destRect.Y + destRect.Height), Colors.Transparent);
+
+            // Draw the new frame
+            WriteableBitmap writeableFrame = new WriteableBitmap(frames[currentFrame]);
+            writeableBitmap.Blit(destRect, writeableFrame, new Rect(0, 0, writeableFrame.PixelWidth, writeableFrame.PixelHeight));
+        }
+
+
+    }
+
+}
---
+++ MEngine2d/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MEngine2d
+{
+    public partial class MainWindow : Window
+    {
+        private WriteableBitmap _writeableBitmap;
+        private IntPtr _buffer;
+        private List<AnimatedEntity> _entities;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _writeableBitmap = new WriteableBitmap(800, 600, 96, 96, PixelFormats.Bgra32, null);
+            _buffer = _writeableBitmap.BackBuffer;
+            BitmapImage spriteSheet = new BitmapImage(new Uri("pack://application:,,,/PixelCrawler/Heroes/Knight/Idle/Idle-Sheet.png")
+);
+
+
+            _entities = new List<AnimatedEntity>();
+            AnimatedEntity player = new AnimatedEntity(spriteSheet, 32, 32);
+            player.X = 100; // example X coordinate
+            player.Y = 100; // example Y coordinate
+            _entities.Add(player);
+
+
+            CompositionTarget.Rendering += GameLoop;
+        }
+
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            foreach(var entity in _entities)
+            {
+                entity.Update();
+                entity.Animate();
+                entity.Draw(_writeableBitmap, entity.X, entity.Y); // Assuming X and Y properties exist
+            }
+
+            // Render to the window
+            myImage.Source = _writeableBitmap;
+        }
+
+    }
+}
